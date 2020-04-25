ebb51722a60db622ae0ca8a55a645f0f81843354
ih <ih@smarthead.ru>
2020-04-25 13:40:19 +0300

    Added test_key_parser.py

--- rest_api_tests_hw/test_key_parser.py
+++ rest_api_tests_hw/test_key_parser.py
@@ -1,15 +1,7 @@
-"""4. Реализуйте в отдельном модуле (файле) тестовую функцию которая будет принимать 2 параметра:
-url - должно быть значение по умолчанию https://ya.ru
-status_code - значение по умолчанию 200
-Параметры должны быть реализованы через pytest.addoption.
-Можно положить фикcтуру и тестовую функцию в один файл.
-Основная задача чтобы ваш тест проверял по переданному урлу статус ответа тот который передали,
-т.е. по адресу https://ya.ru/sfhfhfhfhfhfhfhfh должен быть валидным ответ 404
-
-пример запуска pytest test_module.py --url=https://mail.ru --status_code=200"""
-
 import pytest
 import requests
+
+#pytest test_key_parser.py --url=https://ya.ru/sfhfhfhfhfhfhfhfh --status_code=404
 
 
 @pytest.fixture(scope="session")
